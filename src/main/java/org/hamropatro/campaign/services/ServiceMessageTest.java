e16c01eea9570ca88104e30479a937265f8602e7
Deependra <dbohara@hamropatro.com>
2023-07-29 22:01:42 +0545

    Service Message Refactor

--- src/main/java/org/hamropatro/campaign/services/ServiceMessageTest.java
+++ src/main/java/org/hamropatro/campaign/services/ServiceMessageTest.java
@@ -2,6 +2,7 @@ package org.hamropatro.campaign.services;
 
 import org.hamropatro.util.CustomListener;
 import org.hamropatro.util.LoginUtil;
+import org.hamropatro.util.Repository;
 import org.hamropatro.util.ScreenShots;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -22,43 +23,44 @@ public class ServiceMessageTest {
         driver.switchTo().window(loginUtil.getParentHandle());
     }
     public void createServiceMessage() throws InterruptedException {
+        Repository locator= new Repository();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         loginUtil.Login();
         parentHandle();
-        WebElement myElement = driver.findElement(By.id("theme-btn"));
+        WebElement myElement = driver.findElement(By.id(locator.getCreateCampaignButton()));
         myElement.click();
         System.out.println("Campaign button is clicked");
 
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div/div[1]/div[1]/div/div[2]")).click();
+        driver.findElement(By.xpath(locator.getClickOnServiceMessage())).click();
         System.out.println("Service Message is selected");
         Thread.sleep(5000);
 
         WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(15));
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div[1]/form/div/div[2]/div/div/div/div/div/div/label[2]/span[1]/input")).click();
+        driver.findElement(By.xpath(locator.getSelectTarget()));
         System.out.println("User list is selected");
         Thread.sleep(3000);
 
-        WebElement dropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div[1]/form/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div/div/div/div/div/span[1]/input")));
+        WebElement dropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator.getSelectAudience())));
         dropdown.sendKeys("To me");
         dropdown.sendKeys(Keys.ENTER);
         System.out.println("Option 'To me' is selected");
         Thread.sleep(3000);
 
-        WebElement scheduled = driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div[1]/form/div/div[3]/div/div[2]/div[1]/div/div/div/div/div/label/span[1]/input"));
+        WebElement scheduled = driver.findElement(By.xpath(locator.getClickOnScheduleSM()));
         scheduled.click();
         System.out.println("Schedule button is clicked");
 
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div[1]/form/div/div[3]/div/div[1]/div[2]/div/div/div/div/div/label/span[1]/input")).click();
+        driver.findElement(By.xpath(locator.getCLickOnDryRunSM())).click();
         System.out.println("Dry run is clicked");
 
-        driver.findElement(By.id("subtitle")).sendKeys("This is a Subtitle");
+        driver.findElement(By.id(locator.getEnterSubTitle())).sendKeys("This is a Subtitle");
         System.out.println("Subtitle is entered");
 
-        WebElement title=driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div[1]/form/div/div[5]/div/div[2]/div/div/div[2]/div[1]/div/input"));
+        WebElement title=driver.findElement(By.xpath(locator.getEnterSmTitle()));
         title.sendKeys("This is a Title");
         System.out.println("Title is entered");
 
-        WebElement dropDown= driver.findElement(By.id("miniAppId"));
+        WebElement dropDown= driver.findElement(By.id(locator.getChooseMiniApp()));
         dropDown.sendKeys("2");
         dropDown.sendKeys(Keys.ENTER);
         System.out.println("Mini App is selected");
@@ -66,13 +68,13 @@ public class ServiceMessageTest {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 
-        WebElement button=driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div[1]/form/div/div[6]/div/div/div/div/div/div/div/div[1]/button"));
+        WebElement button=driver.findElement(By.xpath(locator.getCreateSmCampaign()));
         button.click();
         System.out.println("Create campaign button is clicked");
 
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement popoverElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[4]/div/div/div/div[2]/div")));
-        WebElement confirmButton = popoverElement.findElement(By.xpath("/html/body/div[4]/div/div/div/div[2]/div/div[2]/button[2]"));
+        WebElement popoverElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator.getChooseConfirmBox())));
+        WebElement confirmButton = popoverElement.findElement(By.xpath(locator.getClickOnSMYes()));
         confirmButton.click();
         System.out.println("Confirmed Yes");
         System.out.println("Service message is created successfully");
@@ -88,30 +90,30 @@ public class ServiceMessageTest {
     }
     @Test
     public void verifyServiceMessage() {
-
+        Repository locator= new Repository();
         String expectedTarget = "To me UL";
-        String actualTarget = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div/div/div/div/div/div/table/tbody/tr[2]/td[1]")).getText();
+        String actualTarget = driver.findElement(By.xpath(locator.getConfirmTarget())).getText();
         System.out.println("Actual target :"+actualTarget);
         Assert.assertEquals(actualTarget, expectedTarget, "Campaign  target does not match");
 
         String expectedStatus = "DRY";
-        String actualStatus = driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div/div/div/div/div/div/table/tbody/tr[2]/td[2]/div/div/div/span")).getText();
+        String actualStatus = driver.findElement(By.xpath(locator.getConfirmStatus())).getText();
         System.out.println("Actual Status :"+actualStatus);
         Assert.assertEquals(actualStatus,expectedStatus, "Status does not match");
 
         String expectedTitle = "DRYHamro Gifts - This is a Title";
-        WebElement element = driver.findElement(By.xpath( "/html/body/div[1]/div/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div/div/div/div/div/div/table/tbody/tr[2]/td[2]/div/div/div"));
+        WebElement element = driver.findElement(By.xpath( locator.getConfirmTitle()));
         String actualTitle = element.getText();
         System.out.println("Actual Title :"+actualTitle);
         Assert.assertEquals(actualTitle, expectedTitle, "Campaign title does not match");
 
         String expectedType = "SM";
-        String actualType = driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div/div/div/div/div/div/table/tbody/tr[2]/td[3]")).getText();
+        String actualType = driver.findElement(By.xpath(locator.getConfirmServiceType())).getText();
         Assert.assertEquals(actualType,expectedType, "Campaign type does not match");
         System.out.println("Actual Type :"+actualType);
 
         String expectedAuthor = "dbohara@hamropatro.com";
-        String actualAuthor = driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div/div/div/div/div/div/table/tbody/tr[2]/td[10]")).getText();
+        String actualAuthor = driver.findElement(By.xpath(locator.getConfirmAuthor())).getText();
         Assert.assertEquals(actualAuthor,expectedAuthor, "Author name does not match");
         System.out.println("Actual Author :"+actualAuthor);
     }
--- src/main/java/org/hamropatro/util/Repository.java
+++ src/main/java/org/hamropatro/util/Repository.java
@@ -77,13 +77,79 @@ public class Repository {
     public String getAuthor() {
         return Author;
     }
+    public String getCreateCampaignButton() {
+        return CreateCampaignButton;
+    }
+
+    public String getClickOnServiceMessage() {
+        return ClickOnServiceMessage;
+    }
+
+    public String getSelectTarget() {
+        return SelectTarget;
+    }
+
+    public String getSelectAudience() {
+        return SelectAudience;
+    }
+
+    public String getClickOnScheduleSM() {
+        return ClickOnScheduleSM;
+    }
+
+    public String getCLickOnDryRunSM() {
+        return CLickOnDryRunSM;
+    }
+
+    public String getEnterSmTitle() {
+        return EnterSmTitle;
+    }
+    public String getEnterSubTitle() {
+        return EnterSubTitle;
+    }
+
+    public String getChooseMiniApp() {
+        return ChooseMiniApp;
+    }
+
+    public String getCreateSmCampaign() {
+        return CreateSmCampaign;
+    }
+
+    public String getChooseConfirmBox() {
+        return ChooseConfirmBox;
+    }
+
+    public String getClickOnSMYes() {
+        return ClickOnSMYes;
+    }
+
+    public String getConfirmTarget() {
+        return ConfirmTarget;
+    }
+
+    public String getConfirmStatus() {
+        return ConfirmStatus;
+    }
+
+    public String getConfirmTitle() {
+        return ConfirmTitle;
+    }
+
+    public String getConfirmServiceType() {
+        return ConfirmServiceType;
+    }
+
+    public String getConfirmAuthor() {
+        return ConfirmAuthor;
+    }
     private String ContinueWithGoogle;
     private String EnterEmail;
-    private  String ClickEmailButton;
+    private String ClickEmailButton;
     private String EnterPassword;
     private String ClickButton;
     private String ClickOnCreateCampaign;
-    private  String ChooseTarget;
+    private String ChooseTarget;
     private String ClickOnSchedule;
     private String CLickOnDryRun;
     private String EnterTitle;
@@ -97,6 +163,24 @@ public class Repository {
     private String CampaignTitle;
     private String CampaignType;
     private String Author;
+    private String CreateCampaignButton;
+    private String ClickOnServiceMessage;
+    private String SelectTarget;
+    private String SelectAudience;
+    private String ClickOnScheduleSM;
+    private String CLickOnDryRunSM;
+    private String EnterSmTitle;
+    private String EnterSubTitle;
+    private String ChooseMiniApp;
+    private String CreateSmCampaign;
+    private String ChooseConfirmBox;
+    private String ClickOnSMYes;
+    private String ConfirmTarget;
+    private String ConfirmStatus;
+    private String ConfirmTitle;
+    private String ConfirmServiceType;
+    private String ConfirmAuthor;
+
 
     public Repository(){
 
@@ -111,6 +195,7 @@ public class Repository {
             this.ClickEmailButton= locate.getProperty("ClickEmailButton");
             this.EnterPassword = locate.getProperty("EnterPassword");
             this.ClickButton = locate.getProperty("ClickButton");
+            //GP
             this.ClickOnCreateCampaign = locate.getProperty("ClickOnCreateCampaign");
             this.ChooseTarget=locate.getProperty("ChooseTarget");
             this.ClickOnSchedule = locate.getProperty("ClickOnSchedule");
@@ -126,6 +211,26 @@ public class Repository {
             this.CampaignTitle = locate.getProperty("CampaignTitle");
             this.CampaignType = locate.getProperty("CampaignType");
             this.Author = locate.getProperty("Author");
+            //SM
+            this.CreateCampaignButton=locate.getProperty("CreateCampaignButton");
+            this.ClickOnServiceMessage= locate.getProperty("ClickOnServiceMessage");
+            this.SelectTarget = locate.getProperty("SelectTarget");
+            this.SelectAudience = locate.getProperty("SelectAudience");
+            this.ClickOnScheduleSM = locate.getProperty("ClickOnScheduleSM");
+            this.CLickOnDryRunSM = locate.getProperty("CLickOnDryRunSM");
+            this.EnterSmTitle = locate.getProperty("EnterSmTitle");
+            this.EnterSubTitle = locate.getProperty("EnterSubTitle");
+            this.ChooseMiniApp = locate.getProperty("ChooseMiniApp");
+            this.CreateSmCampaign = locate.getProperty("CreateSmCampaign");
+            this.ChooseConfirmBox = locate.getProperty("ChooseConfirmBox");
+            this.ClickOnSMYes = locate.getProperty("ClickOnSMYes");
+            this.ConfirmTarget = locate.getProperty("ConfirmTarget");
+            this.ConfirmStatus = locate.getProperty("ConfirmStatus");
+            this.ConfirmTitle = locate.getProperty("ConfirmTitle");
+            this.ConfirmServiceType = locate.getProperty("ConfirmServiceType");
+            this.ConfirmAuthor = locate.getProperty("ConfirmAuthor");
+
+
         } catch (
                 IOException ex) {
             ex.printStackTrace();
